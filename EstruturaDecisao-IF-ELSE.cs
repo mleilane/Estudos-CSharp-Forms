622c49c9a79311f71a936fd17f9846daf02832b1
mleilane <leilane94silva@yahoo.com>
2024-10-17 18:42:20 -0300

    Feat: implementar lógica para exibir o dia da semana em portuguÊs
    - Adicionado o uso da estrutura switch-case  para verificar o dia da semana
    
    - Implementadoo uso da estrutura if-else  para verificar o dia da semana
    
    - Ambas exibem corretamente o dia da semana em português

---
+++ EstruturaDecisao-IF-ELSE.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+
+// criando um sistema para aprovação ou reprovação de aluno
+
+namespace EstruturaRepeticao
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string nome;
+            double nota1, nota2, nota3, media;
+
+            Console.WriteLine("Digite o nome do Aluno: ");
+            nome = Console.ReadLine();
+
+
+            Console.WriteLine("Digite a nota1 do aluno: ");
+            nota1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a nota2 do aluno: ");
+            nota2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a nota3 do aluno: ");
+            nota3 = double.Parse(Console.ReadLine());
+
+            media = (nota1 + nota2 + nota3) / 3;
+
+            //Console.WriteLine("Olá, " + nome);
+            Console.WriteLine("Sua nota final foi, " + media);
+
+
+
+            if (media < 5)
+            {
+                Console.WriteLine("Infelizmente você foi REPROVADO (A) " + nome);
+            }
+            else if (media == 5)
+            {
+                Console.WriteLine("Parabéns " + nome + "! Você foi APROVADA, mas foi por pouco.");
+
+            }
+            else
+            {
+                Console.WriteLine("Parabéns " + nome + "! Você foi APROVADA!");
+            }
+        }
+    }
+}
---
+++ EstruturaDecisao-SWITCH-CASE.cs
@@ -0,0 +1,85 @@
+﻿// *******************  RESUMO  ******************* 
+// SWITCH CASE -> Mais indicadO quando temos muitas opções para analiar
+// define a lógica de maneira mais direta, sem a necessidade de comparar cada condição separadamente.
+
+
+// PROJETO -> Criando um programa que avalia o dia da semana, com base no dia atual
+
+
+using System;
+
+namespace Aula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Obtém o dia atual
+            DateTime dataAtual = DateTime.Now;
+
+            // Obtém o dia da semana com base na data atual
+            DayOfWeek diaSemana = dataAtual.DayOfWeek;
+
+
+            // Exemplo com SWITCH CASE
+            switch (diaSemana)
+            {
+                case DayOfWeek.Sunday:
+                    Console.WriteLine("BLOCO DO SWITCH: Hoje é Domingo");
+                    break;
+                case DayOfWeek.Monday:
+                    Console.WriteLine("BLOCO DO SWITCH: Hoje é Segunda-feira");
+                    break;
+                case DayOfWeek.Tuesday:
+                    Console.WriteLine("BLOCO DO SWITCH: Hoje é Terça-feira");
+                    break;
+                case DayOfWeek.Wednesday:
+                    Console.WriteLine("BLOCO DO SWITCH: Hoje é Quarta-feira");
+                    break;
+                case DayOfWeek.Thursday:
+                    Console.WriteLine("BLOCO DO SWITCH: Hoje é Quinta-feira");
+                    break;
+                case DayOfWeek.Friday:
+                    Console.WriteLine("BLOCO DO SWITCH: Hoje é Sexta-feira");
+                    break;
+                case DayOfWeek.Saturday:
+                    Console.WriteLine("BLOCO DO SWITCH: Hoje é Sábado");
+                    break;
+            }
+
+
+
+            // exemplo com IF ELSE 
+
+            if (diaSemana == DayOfWeek.Monday)
+            {
+                Console.WriteLine("BLOCO DO IF: Hoje é Segunda-feira");
+            }
+            else if (diaSemana == DayOfWeek.Tuesday)
+            {
+                Console.WriteLine("BLOCO DO IF: Hoje é Terça-feira");
+            }
+            else if (diaSemana == DayOfWeek.Wednesday)
+            {
+                Console.WriteLine("BLOCO DO IF: Hoje é Quarta-feira");
+            }
+            else if (diaSemana == DayOfWeek.Thursday)
+            {
+                Console.WriteLine("BLOCO DO IF: Hoje é Quinta-feira");
+            }
+            else if (diaSemana == DayOfWeek.Friday)
+            {
+                Console.WriteLine("BLOCO DO IF: Hoje é Sexta-feira");
+            }
+            else if (diaSemana == DayOfWeek.Saturday)
+            {
+                Console.WriteLine("BLOCO DO IF: Hoje é Sábado-feira");
+            }
+            else
+            {
+                Console.WriteLine("BLOCO DO IF: Hoje é Domingo");
+            }
+        }
+    }
+}
